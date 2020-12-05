81f4d22024761cfee15eb7bb52bbd25783393a9c
Scotty Pate <scottypate@me.com>
2020-12-05 10:30:08 -0600

    Add formatting

--- 2020/day5.go
+++ 2020/day5.go
@@ -5,8 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
-	"strings"
 	"sort"
+	"strings"
 )
 
 // Read the input file.
@@ -25,90 +25,90 @@ func getInput() []string {
 }
 
 func makeSlice(min int, max int) []int {
-    s := make([]int, max-min+1)
-    for i := range s {
-        s[i] = min + i
-    }
-    return s
+	s := make([]int, max-min+1)
+	for i := range s {
+		s[i] = min + i
+	}
+	return s
 }
 
 func halveSlice(s []int, half string) []int {
-    middle := len(s) / 2
-    var returnVal []int
+	middle := len(s) / 2
+	var returnVal []int
 
-    if half == "lower" {
-    	returnVal = s[:middle]
-    } else if half == "upper" {
-    	returnVal = s[middle:]
-    }
-    return returnVal
+	if half == "lower" {
+		returnVal = s[:middle]
+	} else if half == "upper" {
+		returnVal = s[middle:]
+	}
+	return returnVal
 }
 
 func calculateRow(instruction string) int {
-        rows := makeSlice(0, 127)
-        var half string
-        for i := 0; i < 7; i++ {
-            if string(instruction[i]) == "F" {
-            	half = "lower"
-            } else if string(instruction[i]) == "B" {
-            	half = "upper"
-            }
-            rows = halveSlice(rows, half)
-        }
-        return rows[0]
+	rows := makeSlice(0, 127)
+	var half string
+	for i := 0; i < 7; i++ {
+		if string(instruction[i]) == "F" {
+			half = "lower"
+		} else if string(instruction[i]) == "B" {
+			half = "upper"
+		}
+		rows = halveSlice(rows, half)
+	}
+	return rows[0]
 }
 
 func calculateSeat(instruction string) int {
-        seats := makeSlice(0, 7)
-        var half string
-        for i := 7; i < 10; i++ {
-            if string(instruction[i]) == "L" {
-            	half = "lower"
-            } else if string(instruction[i]) == "R" {
-            	half = "upper"
-            }
-            seats = halveSlice(seats, half)
-        }
-        return seats[0]
+	seats := makeSlice(0, 7)
+	var half string
+	for i := 7; i < 10; i++ {
+		if string(instruction[i]) == "L" {
+			half = "lower"
+		} else if string(instruction[i]) == "R" {
+			half = "upper"
+		}
+		seats = halveSlice(seats, half)
+	}
+	return seats[0]
 }
 
 // Figure out highest seat ID in the list of boarding passes
 func partOne(inputVal []string) int {
 	var seatIds []int
-        for _, element := range inputVal {
-        	row := calculateRow(element)
-        	seat := calculateSeat(element)
-        	seatId := (row * 8) + seat
-        	seatIds = append(seatIds, seatId)
-        }
-        sort.Ints(seatIds)
-        return seatIds[len(seatIds) - 1]
+	for _, element := range inputVal {
+		row := calculateRow(element)
+		seat := calculateSeat(element)
+		seatId := (row * 8) + seat
+		seatIds = append(seatIds, seatId)
+	}
+	sort.Ints(seatIds)
+	return seatIds[len(seatIds)-1]
 }
 
 // Find the missing seat ID in the list.
 func partTwo(inputVal []string) int {
 	var seatIds []int
 	var mySeat int
-        for _, element := range inputVal {
-        	row := calculateRow(element)
-        	seat := calculateSeat(element)
-        	seatId := (row * 8) + seat
-        	seatIds = append(seatIds, seatId)
-        }
-        sort.Ints(seatIds)
-        min := seatIds[0]
-        max := seatIds[len(seatIds) - 1]
+	for _, element := range inputVal {
+		row := calculateRow(element)
+		seat := calculateSeat(element)
+		seatId := (row * 8) + seat
+		seatIds = append(seatIds, seatId)
+	}
+	sort.Ints(seatIds)
+	min := seatIds[0]
+	max := seatIds[len(seatIds)-1]
 
-        for i := min; i < max; i++ {
-        	searchResult := sort.SearchInts(seatIds, i)
-        	if searchResult < len(seatIds) && seatIds[searchResult] == i {
-        		continue
-        	} else {
-        		mySeat = i
-        	}
-        }
+	for i := min; i < max; i++ {
+		searchResult := sort.SearchInts(seatIds, i)
+		if searchResult < len(seatIds) && seatIds[searchResult] == i {
+			continue
+		} else {
+			mySeat = i
+		}
+	}
 
-        return mySeat
+	return mySeat
 }
 
 func main() {
